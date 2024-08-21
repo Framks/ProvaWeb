0bb6c637c7d97983b0ebab94a94472d60123a102
Francisco <galvesa9@gmail.com>
2024-08-21 08:20:35 -0300

    Init prova

---
+++ src/Components/Questao1.jsx
@@ -0,0 +1,17 @@
+const Questao01A = () => {
+    const lista = [
+        {a:10, b:3, c: 7},
+        {a:5, b:-3, c: 9},
+        {a:1, b:9, c: 40}
+    ]
+    
+    return(
+        <Questao01B lista={lista}/>
+    )
+}
+
+function Questao01B(lista){
+
+}
+
+export default {Questao01A, Questao01B};
